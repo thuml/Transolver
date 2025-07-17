238069925e21899d4d89981a3a192f253f05f6fb
mae-zhangbj <1002129979@qq.com>
2025-07-17 13:38:48 +0800

    Usage_Python/Usage_Transolver_Structured_Mesh_2D.py update

--- PDE-Solving-StandardBenchmark/My_python_job/Usage_Python/Usage_Transolver_Structured_Mesh_2D.py
+++ PDE-Solving-StandardBenchmark/My_python_job/Usage_Python/Usage_Transolver_Structured_Mesh_2D.py
@@ -1,7 +1,12 @@
 """
+
 Usage of Transolver_Structured_Mesh_2D.py
+
 """
 
 ACTIVATION = {'gelu': nn.GELU, 'tanh': nn.Tanh, 'sigmoid': nn.Sigmoid, 'relu': nn.ReLU, 'leaky_relu': nn.LeakyReLU(0.1),
               'softplus': nn.Softplus, 'ELU': nn.ELU, 'silu': nn.SiLU}
+
+#!-----------------------------------------------------------------------
+class Model(nn.Module):
 
