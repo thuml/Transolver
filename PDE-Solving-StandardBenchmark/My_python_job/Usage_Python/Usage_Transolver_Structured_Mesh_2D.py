fab83feb67596b6c99155d5d6d4653871549e25a
mae-zhangbj <1002129979@qq.com>
2025-07-21 10:41:58 +0800

    Usage_Python/Usage_Transolver_Structured_Mesh_2D.py update

--- PDE-Solving-StandardBenchmark/My_python_job/Usage_Python/Usage_Transolver_Structured_Mesh_2D.py
+++ PDE-Solving-StandardBenchmark/My_python_job/Usage_Python/Usage_Transolver_Structured_Mesh_2D.py
@@ -10,3 +10,22 @@ ACTIVATION = {'gelu': nn.GELU, 'tanh': nn.Tanh, 'sigmoid': nn.Sigmoid, 'relu': n
 #!-----------------------------------------------------------------------
 class Model(nn.Module):
 
+#!-----------------------------------------------------------------------
+class MLP(nn.Module):
+
+#!---------------------
+   def get_grid(self, batchsize=1):
+
+        grid = torch.cat((gridx, gridy), dim=-1).cuda()  # B H W 2
+        -> Concatenate them along the last dimension
+        -> Example:
+            -> gridx.shape = [B, H, W, 1]
+            -> gridy.shape = [B, H, W, 1]
+            -> grid.shape  = [B, H, W, 2]
+
+
+
+""" Variable meaning """
+# fun_dim:              number of physical features per mesh points(e.g. velocity, pressure)
+# self.ref:             refinement
+# Input=fun_dim + self.ref^2 (pos + physical input)
