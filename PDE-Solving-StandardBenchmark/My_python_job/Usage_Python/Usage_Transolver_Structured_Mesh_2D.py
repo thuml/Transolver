10d8c869c6f50e6f8bd095aa088dcdf70dc4aa0e
mae-zhangbj <1002129979@qq.com>
2025-07-21 14:58:12 +0800

    My_python_job/Usage_Python/Usage_Transolver_Structured_Mesh_2D.py .get_grid() update

--- PDE-Solving-StandardBenchmark/My_python_job/Usage_Python/Usage_Transolver_Structured_Mesh_2D.py
+++ PDE-Solving-StandardBenchmark/My_python_job/Usage_Python/Usage_Transolver_Structured_Mesh_2D.py
@@ -10,11 +10,12 @@ ACTIVATION = {'gelu': nn.GELU, 'tanh': nn.Tanh, 'sigmoid': nn.Sigmoid, 'relu': n
 #!-----------------------------------------------------------------------
 class Model(nn.Module):
 
-#!-----------------------------------------------------------------------
-class MLP(nn.Module):
-
-#!---------------------
+#!--------------------------------------------
    def get_grid(self, batchsize=1):
+   -> Capture the relative geometry
+   -> Have more features
+   -> reference grid is self.ref * self.ref
+   -> mesh grid is self.H * self.W
 
         grid = torch.cat((gridx, gridy), dim=-1).cuda()  # B H W 2
         -> Concatenate them along the last dimension
@@ -22,10 +23,49 @@ class MLP(nn.Module):
             -> gridx.shape = [B, H, W, 1]
             -> gridy.shape = [B, H, W, 1]
             -> grid.shape  = [B, H, W, 2]
+        -> We can get point-wise pair by grid[0, x_i, y_i]
 
+#!---------------------
+        diff = grid[:, :, :, None, None, :] - grid_ref[:, None, None, :, :, :]
+        -> grid[:, :, :, None, None, :]
+           -> Pytorch automatically broadcasts the tensor "grid" shape into [B, H, W, ref, ref, 2]
+           -> Now the channel is still two
+        -> grid_ref[:, None, None, :, :, :]
+           -> Same operation
+           -> grid_ref.shape = [B, H, W, ref, ref, 2]
+        -> Now "grid" and "grid_ref" have the same shape, and are okey to have some math opreation
+        -> diff.shape = [B, H, W, ref, ref, 2]
+           -> "diff" stores dx and dy between mesh points and reference points
+
+#!---------------------
+        torch_sum = torch.sum(diff**2, dim=-1)
+        -> torch_sum.shape = [B, H, W, ref, ref]
+           -> It stores dx^2 + dy^2
+
+#!---------------------
+        pos = sqrt.reshape(batchsize, size_x, size_y, self.ref * self.ref).contiguous()
+        -> pos.shape = [B, H, W, ref*ref]
+           -> H: mesh-grid row
+           -> W: mesh-grid column
+           -> ref*ref: flattened reference-grid index
+        -> How to access the distance beween mehs_point(aa, bb) and ref_point(ii, jj)
+           -> ref_idx = ii * self.ref + jj
+           -> {B, aa, bb, ref_idx}
+
+
+#!-----------------------------------------------------------------------
+class MLP(nn.Module):
+
+#!-----------------------------------------------------------------------
+    def __init__(self, n_input, n_hidden, n_output, n_layers=1, act='gelu', res=True):
+    -> Example with n_layers
+        -> Keyword argu: MLP(n_layers=0)
+        -> Positional argu: MLP(0)
+        -> If we use Keyword argu, the passing order can be mixed
+        -> Positional argu is permitted
 
 
 """ Variable meaning """
-# fun_dim:              number of physical features per mesh points(e.g. velocity, pressure)
-# self.ref:             refinement
-# Input=fun_dim + self.ref^2 (pos + physical input)
+# fun_dim:                            number of physical features per mesh points(e.g. velocity, pressure)
+# self.ref:                           reference grid points
+# Input=fun_dim + self.ref^2          (pos + physical input)
